7af4641929e7c6a75eb8d70e149536f977627519
Gary <gary@elasticdev.io>
2021-09-24 16:05:37 +0800

    ''

--- execgroups/_ed_configs/on_docker/_rules/order.py
+++ execgroups/_ed_configs/on_docker/_rules/order.py
@@ -1,7 +1,7 @@
 def default():
 
     ordered_tasks=[]
-    ordered_tasks.append("15-add_ansible_sshkey.py,end")
-    ordered_tasks.append("25-setup_ansible.py,end")
+    ordered_tasks.append("15-ansible_sshkey.py,end")
+    ordered_tasks.append("25-ansible_hosts.py,end")
 
     return ordered_tasks
